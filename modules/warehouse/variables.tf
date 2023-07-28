9b22b5488ed44522fc6e640fcb7e920d57faa08e
David Schwartz <dschwartz0815@gmail.com>
2023-07-28 17:25:31 -0400

    added modules

---
+++ modules/warehouse/variables.tf
@@ -0,0 +1,82 @@
+variable "warehouse_name" {
+  type = string
+}
+
+variable "warehouse_comment" {
+  type    = string
+  default = ""
+}
+
+variable "warehouse_size" {
+  type    = string
+  default = "XSMALL"
+}
+
+variable "warehouse_auto_resume" {
+  type    = bool
+  default = true
+}
+
+variable "warehouse_auto_suspend" {
+  type    = number
+  default = 60
+}
+
+variable "warehouse_initially_suspended" {
+  type    = bool
+  default = true
+}
+
+variable "warehouse_max_cluster_count" {
+  type    = number
+  default = 1
+}
+
+variable "warehouse_min_cluster_count" {
+  type    = number
+  default = 1
+}
+
+variable "warehouse_scaling_policy" {
+  type    = string
+  default = "ECONOMY"
+}
+
+variable "warehouse_grant_roles" {
+  type = map(any)
+}
+
+variable "warehouse_grant_with_grant_option" {
+  type    = bool
+  default = false
+}
+
+variable "resource_monitor_credit_quota" {
+  type    = number
+  default = 25
+}
+
+variable "resource_monitor_frequency" {
+  type    = string
+  default = "DAILY"
+}
+
+variable "resource_monitor_start_timestamp" {
+  type    = string
+  default = "IMMEDIATELY"
+}
+
+variable "resource_monitor_notify_triggers" {
+  type    = list(number)
+  default = [80]
+}
+
+variable "resource_monitor_suspend_triggers" {
+  type    = list(number)
+  default = [100]
+}
+
+variable "resource_monitor_suspend_immediate_triggers" {
+  type    = list(number)
+  default = [150]
+}
